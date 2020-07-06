b52b1eec91b0055762c7a396d46e026be6444cc2
chase1635321 <chasekanipe@gmail.com>
2020-07-06 13:40:51 -0400

    First commit

---
+++ src/core/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+// Initialize the core module
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
